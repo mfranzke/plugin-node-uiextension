99957f30d25fd3f1a01ab798fbc54382d9a868bf
Maximilian <787658+mfranzke@users.noreply.github.com>
2021-11-10 12:06:15 +0100

    refactor: added postinstall and optimized testing scripts

---
+++ postinstall.js
@@ -0,0 +1,6 @@
+console.log(`Pattern Lab Node Plugin - "@mfranzke/plugin-node-uiextension" installed.
+Use the CLI to install this: patternlab install --plugins @mfranzke/plugin-node-uiextension
+Configure or disable this plugin inside your patternlab-config.json file.
+Add stylesheets, navigation or tool links to the Pattern Lab UI by adding URLs to the "stylesheets" array, or to the "navLinks" or "toolLinks" arrays on the plugins['@mfranzke/plugin-node-uiextension'].options object.
+For further information please have a look at the README file of this plugin.
+`);
